40d178e596c205505665e4ed883c925796f414c0
Eric Ventor <ericvtheg@Erics-MBP.lan>
2023-06-22 19:01:37 -0700

    added hero page

---
+++ app/shows/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import SoundCloudPlayer from "./soundcloud"
+
+export default function Page({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {/* <SoundCloudPlayer /> */}
+    </div >
+    )
+}
